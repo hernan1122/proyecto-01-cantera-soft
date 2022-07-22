9f5325f0d19d955c7675ed24e31c2b5ec2c1363b
Axel Torrico <torricohernan2@gmail.com>
2022-07-22 09:00:51 -0500

    primer commit

---
+++ algoritmos.js
@@ -0,0 +1,123 @@
+let n1 = 10;
+let n2 = 20;
+
+const sumar = a + b;
+
+if (resultado > 30) {
+    return console.log("El valor es mayor que 3")
+} else {
+    return console.log("El valor es menor que 3")
+}
+
+function sumar (){
+let n1 = 10;
+let n2 = 20;
+
+const sumar = a + b;
+
+if (resultado > 30) {
+    return console.log("El valor es mayor que 30")
+} else {
+    return console.log("El valor es menor que 30")
+}}
+
+
+
+let a = 5;
+let b = 8;
+
+const sumar = a + b;
+
+if (resultado > 15) {
+    return console.log ("el valor es mayor que 15")
+} else {
+    return console.log ("el valor es menor que 15")
+}
+
+function resultado (){
+    let a = 5;
+let b = 8;
+
+const sumar = a + b;
+
+if (resultado > 15) {
+    return console.log ("el valor es mayor que 15")
+} else {
+    return console.log ("el valor es menor que 15")
+}
+}
+
+
+let x = 3;
+let v = 4;
+
+const sumar = x + v;
+
+if (resultado > 7) {
+    return console.log ("el valor es mayor que 7")
+} else {
+    return console.log ("el valor es mayor que 7")
+}
+
+function resultado (){
+    let x = 3;
+    let v = 4;
+    
+    const sumar = x + v;
+    
+    if (resultado > 7) {
+        return console.log ("el valor es mayor que 7")
+    } else {
+        return console.log ("el valor es mayor que 7")
+    } 
+}
+
+
+let n = 20;
+let m = 10;
+
+const sumar = n + m;
+
+if (resultado > 30) {
+    return console.log ("el valor es mayor que 30")
+} else {
+    return console.log ("el valor es menor que 30")
+}
+
+function resultado (){
+    let n = 20;
+    let m = 10;
+    
+    const sumar = n + m;
+    
+    if (resultado > 30) {
+        return console.log ("el valor es mayor que 30")
+    } else {
+        return console.log ("el valor es menor que 30")
+    } 
+}
+
+
+let d = 5;
+let e = 4;
+
+const sumar = n1 + n2;
+
+if (resultado > 9) {
+    return console.log ("el valor es mayor que 9")
+} else {
+    return console.log ("el valor es menor que 9")
+}
+
+function resultado (){
+    let n1 = 5;
+    let n2 = 4;
+    
+    const suma = n1 + n2;
+    
+    if (resultado > 9) {
+        return console.log ("el valor es mayor que 9")
+    } else {
+        return console.log ("el valor es menor que 9")
+    }   
+}
